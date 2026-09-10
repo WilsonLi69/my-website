52db3f58c83909860c44d5e4b3e62bbc459f49b3
wilsonli <44079152+WilsonLi69@users.noreply.github.com>
2026-09-10 20:10:53 +0800

    conf: Modify base URL and project name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://WilsonLi69.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/wiki/',
+  baseUrl: '/my-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'WilsonLi69', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'my-website', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
