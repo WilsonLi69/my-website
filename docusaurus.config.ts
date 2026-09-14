41f5a1ec616221c4f78cb9fef01d7616da580db5
Wilson Li <wilson.li@aicpic.com.tw>
2026-09-14 15:59:32 +0800

    misc: remove Docusaurus blog and tutorial template content

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,17 +42,6 @@ const config: Config = {
         docs: {
           sidebarPath: './sidebars.ts',
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -79,13 +68,12 @@ const config: Config = {
           position: 'left',
           label: 'Notes',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
       ],
     },
     footer: {
       style: 'dark',
       links: [
-        
+
       ],
       copyright: `Copyright © 2024-${new Date().getFullYear()} Wilson Li. Licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">CC BY-NC-SA 4.0</a>.`,
     },
